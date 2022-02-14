be7a2973a153f7e9d4f8a7e23ee8c9e60ff18523
imehsans <mehsanjadoon786@gmail.com>
2022-02-14 16:19:31 +0500

    Porfolio site

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,34 @@
+import "./intro.css"
+
+const intro = () => {
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, My Name is </h2>
+                    <h1 className="i-name">Ehsanullah</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">UI/UX Designer</div>
+                            <div className="i-title-item">ReactJs Developer</div>
+                            <div className="i-title-item">Software Engineer</div>
+                            <div className="i-title-item">MERN Stack Developer</div>
+                        </div>
+                    </div>
+                    <div className="i-desc">
+                    Innovative Programmer to make the world a more unified. A creative thinker, adept in software development and
+working with various data structures and Design pattern's.
+                    </div>
+                </div>
+            </div>
+            <div className="i-Right">
+            <div className="i-right-wrapper">
+                    <h2>Hello My Name is Ehsan</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default intro
